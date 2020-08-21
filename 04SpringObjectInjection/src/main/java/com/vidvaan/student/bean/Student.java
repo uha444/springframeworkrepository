37c694d77ed14436614c28a1daeb7bf5176b6cda
uhach <uhach@LAPTOP-10PSRGSR>
2020-08-21 13:25:43 +0530

    commit SpringObjectInjection

---
+++ 04SpringObjectInjection/src/main/java/com/vidvaan/student/bean/Student.java
@@ -0,0 +1,49 @@
+package com.vidvaan.student.bean;
+
+//dependent
+public class Student {
+	// dependencies
+	private int sid;
+	private String name;
+	private Address address;
+	public Student() {
+		
+	}
+
+	public Student(int sid, String name, Address address) {
+		super();
+		this.sid = sid;
+		this.name = name;
+		this.address = address;
+	}
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", name=" + name + ", address=" + address + "]";
+	}
+
+}
---
+++ 04SpringObjectInjection/src/main/java/com/vidvaan/student/client/StudentClient.java
@@ -0,0 +1,19 @@
+package com.vidvaan.student.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.vidvaan.student.bean.Student;
+
+public class StudentClient {
+	public static void main(String[] args) {
+		
+		ApplicationContext cont = new ClassPathXmlApplicationContext("spring.xml");
+		Student student = cont.getBean("student", Student.class);
+		Student student2 = cont.getBean("student", Student.class);
+		System.out.println(student.toString());
+		System.out.println(student2.toString());
+
+	}
+
+}
